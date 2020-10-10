24ff15c3c2cbbf6633dab3aff68239da7f381575
Gamaliel Toro <argami@gmail.com>
2020-10-10 02:35:34 +0200

    added inserting coins and validate coin denomination

--- src/VendingMachine.php
+++ src/VendingMachine.php
@@ -4,8 +4,21 @@ namespace vending;
 
 class VendingMachine
 {
+    private $valid_coin_denominations = [0.05, 0.10, 0.25, 1];
+
     public function getInsertedAmount() : float
     {
         return 0.0;
+    }
+
+    # if the denomination of the coin is invalid we return
+    # the amount coin
+    public function InsertCoin(float $coin) : float
+    {
+        if (in_array($coin, $this->valid_coin_denominations)) {
+            return 0.0;
+        }
+
+        return $coin;
     }
 }
--- tests/units/VendingMachine.php
+++ tests/units/VendingMachine.php
@@ -14,4 +14,18 @@ class VendingMachine extends atoum
                 ->float($this->testedInstance->getInsertedAmount())
                 ->isEqualTo(0.0);
     }
+
+    public function testInsertValidCoin()
+    {
+        $vm = $this->newTestedInstance;
+        $this->float($vm->InsertCoin(0.10))->isEqualTo(0.0);
+    }
+
+    public function testInsertInvalidCoin()
+    {
+        $vm = $this->newTestedInstance;
+        $this->float($vm->InsertCoin(0.01))->isEqualTo(0.01);
+        $this->float($vm->InsertCoin(0.11))->isEqualTo(0.11);
+        $this->float($vm->InsertCoin(2.1))->isEqualTo(2.1);
+    }
 }
